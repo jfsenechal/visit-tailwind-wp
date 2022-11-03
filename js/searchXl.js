7fca7bc438c3b02b189c755bd5d80e886cd8bce9
jfsenechal <jfsenechal@gmail.com>
2022-11-03 13:37:53 +0100

    search ok

--- js/searchXl.js
+++ js/searchXl.js
@@ -5,22 +5,27 @@ document.addEventListener('alpine:init', () => {
             noResult: false,
             query: '',
             totalHits: 0,
-            results: null,
+            results: [],
             async launchSearch() {
                 if (this.query === '') return;
                 this.noResults = false;
                 console.log(`search for ${this.query}`);
                 let response = await fetch("https://www.marche.be/api/search.php?s=" + this.query)
-                const t = response.text().then(function (data) {
-                    return data
-                })
-                //   console.log(data)
-                //   this.results = JSON.parse(data)
-                //   this.totalHits = JSON.parse(data).length
-                const data = (JSON.parse(await t))
-                this.results = []
-                this.totalHits = data.length
-                return this;
+                    .then(function (response) {
+                        // The API call was successful!
+                        return response.json();
+                    })
+                    .then(function (data) {
+                        // This is the JSON from our response
+                        return data;
+                    })
+                    .catch(function (err) {
+                        // There was an error
+                        console.warn("Something went wrong.", err);
+                        return err
+                    });
+                this.results = response
+                this.totalHits = response.length
             }
         })
     )
