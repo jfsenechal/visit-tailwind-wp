334c9b314414d4e9fe92f8a9c7af2a01cdb633a5
jfsenechal <jfsenechal@gmail.com>
2022-11-06 21:23:04 +0100

    add main css

--- header.php
+++ header.php
@@ -15,22 +15,6 @@ $locale = LocaleHelper::getSelectedLanguage();
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="profile" href="https://gmpg.org/xfn/11">
 
-        <script type="module" crossorigin src="/assets/agenda.df3fe75a.js"></script>
-        <script type="module" crossorigin src="/assets/article.87ed2af1.js"></script>
-        <link rel="modulepreload" crossorigin href="/assets/Cadre.e341a59e.js">
-        <link rel="modulepreload" crossorigin href="/assets/folder.bd6a1526.js">
-        <link rel="modulepreload" crossorigin href="/assets/Footer.2daf29a2.js">
-        <link rel="stylesheet" href="/assets/Footer.3d49acf8.css">
-        <link rel="stylesheet" href="/assets/home.9a649143.css">
-        <link rel="modulepreload" crossorigin href="/assets/home.64b52e82.js">
-        <link rel="stylesheet" href="/assets/input.07fc8010.css">
-        <link rel="stylesheet" href="/assets/offre.431ac77a.css">
-        <link rel="modulepreload" crossorigin href="/assets/offre.431ac77a.css">
-        <link rel="modulepreload" crossorigin href="/assets/offres.a47128ff.js">
-        <link rel="modulepreload" crossorigin href="/assets/offres.a47128ff.js">
-        <link rel="modulepreload" crossorigin href="/assets/posts.afb79d4b.js">
-        <link rel="modulepreload" crossorigin href="/assets/SeeAlso.b7d944b9.js">
-
         <script src="https://visit.marche.be/wp-content/themes/visittail/assets/js/searchXl.js" defer></script>
         <script src="https://visit.marche.be/wp-content/themes/visittail/assets/js/menuMobile.js" defer></script>
         <script src="https://visit.marche.be/wp-content/themes/visittail/assets/js/refreshOffres.js" defer></script>
