47b791cee8198561834acc488df828819ea041e3
jfsenechal <jfsenechal@gmail.com>
2022-11-06 15:40:01 +0100

    essais carte

--- 404.php
+++ 404.php
@@ -2,6 +2,18 @@
 
 namespace VisitMarche\ThemeTail;
 
-dd(404);
+use VisitMarche\ThemeTail\Lib\Twig;
+
 get_header();
+
+Twig::rendPage(
+    '@VisitTail/errors/404.html.twig',
+    [
+        'title' => 'post_title',
+        'message' => 'post_title',
+        'url' => '/',
+        'latitude' => '5.342961',
+        'longitude' => '50.226484',
+    ]
+);
 get_footer();
