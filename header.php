a366e48bc6209e2932274153c83bb9a8310622eb
jfsenechal <jfsenechal@gmail.com>
2022-11-06 15:41:08 +0100

    essais carte

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace VisitMarche\ThemeTail;
 
 use VisitMarche\ThemeTail\Inc\Menu;
@@ -7,27 +8,29 @@ use VisitMarche\ThemeTail\Lib\Twig;
 
 $locale = LocaleHelper::getSelectedLanguage();
 ?>
-<!doctype html>
+    <!doctype html>
 <html lang="<?php echo $locale; ?>">
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="profile" href="https://gmpg.org/xfn/11">
-    <script type="module" crossorigin src="/assets/agenda.4cb13ffc.js"></script>
-    <link rel="modulepreload" crossorigin href="/assets/Footer.82351317.js">
-    <link rel="modulepreload" crossorigin href="/assets/Cadre.acc3c6eb.js">
-    <link rel="stylesheet" href="/assets/Footer.3d49acf8.css">
-    <link rel="stylesheet" href="/assets/input.c3741591.css">
-    <script src="https://visit.marche.be/wp-content/themes/visittail/assets/js/searchXl.js" defer></script>
-    <script src="https://visit.marche.be/wp-content/themes/visittail/assets/js/menuMobile.js" defer></script>
-    <script src="https://visit.marche.be/wp-content/themes/visittail/assets/js/refreshOffres.js" defer></script>
-    <script type="module" src="https://visit.marche.be/wp-content/themes/visittail/assets/js/map.js" defer></script>
-    <script src="//unpkg.com/alpinejs" defer></script>
-    <?php wp_head(); ?>
-</head>
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="profile" href="https://gmpg.org/xfn/11">
+        <script type="module" crossorigin src="/assets/agenda.4cb13ffc.js"></script>
+        <link rel="modulepreload" crossorigin href="/assets/Footer.82351317.js">
+        <link rel="modulepreload" crossorigin href="/assets/Cadre.acc3c6eb.js">
+        <link rel="stylesheet" href="/assets/Footer.3d49acf8.css">
+        <link rel="stylesheet" href="/assets/input.c3741591.css">
+        <script src="https://visit.marche.be/wp-content/themes/visittail/assets/js/searchXl.js" defer></script>
+        <script src="https://visit.marche.be/wp-content/themes/visittail/assets/js/menuMobile.js" defer></script>
+        <script src="https://visit.marche.be/wp-content/themes/visittail/assets/js/refreshOffres.js" defer></script>
+        <!--
+        <script type="module" src="https://visit.marche.be/wp-content/themes/visittail/assets/js/map.js" defer></script>
+        -->
+        <script src="//unpkg.com/alpinejs" defer></script>
+        <?php wp_head(); ?>
+    </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open();
+    <?php wp_body_open();
 $menu = new Menu();
 $items = $menu->getMenuTop();
 
