dcfacb87d9f9eb1ac5f526b66f92e9565f52ffc5
jfsenechal <jfsenechal@gmail.com>
2022-11-09 11:06:18 +0100

    loading

--- assets/js/alpine/searchXl.js
+++ assets/js/alpine/searchXl.js
@@ -7,25 +7,29 @@ document.addEventListener('alpine:init', () => {
             totalHits: 0,
             results: [],
             async launchSearch() {
-                if (this.query === '') return;
-                this.noResults = false;
-                console.log(`search for ${this.query}`);
-                let response = await fetch("https://www.marche.be/api/search.php?s=" + this.query)
+                if (this.query === '') return
+                this.noResults = false
+                this.isLoading = true
+                console.log(`search for ${this.query}`)
+                let response = await fetch("https://visitmarche.be/api/search.php?s=" + this.query)
                     .then(function (response) {
                         // The API call was successful!
-                        return response.json();
+                        return response.json()
                     })
                     .then(function (data) {
                         // This is the JSON from our response
-                        return data;
+                        return data
                     })
                     .catch(function (err) {
                         // There was an error
-                        console.warn("Something went wrong.", err);
+                        console.warn("Something went wrong.", err)
+                        this.isLoading = false
                         return err
                     });
                 this.results = response
                 this.totalHits = response.length
+                console.log(this.totalHits)
+                this.isLoading = false
             }
         })
     )
